050303e18b4b8996ddbb74f33c6da441966cd582
kuhn (Dr. ) <mxkuhn@gmail.com>
2010-09-19 23:34:35 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qsardata@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/QSARdata/QSARdata_Package_files/QSARdata_Package.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20100919
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('QSARdata_Package_files/QSARdata_PackageMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
